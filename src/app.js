a9065889785540e834386aba913b2c766091f025
Sravani-Kumari-M <160271528+Sravani-Kumari-M@users.noreply.github.com>
2024-10-08 19:37:32 +0530

    Routing basics GET, POST, DELETE methods

--- src/app.js
+++ src/app.js
@@ -2,18 +2,58 @@ const express = require("express");
 
 const app = express(); // we are creating a express js application, creating a new web server
 
-app.use("/", (req, res) => {
-    res.send("Namaste!");
+
+
+app.use("/user", (req, res) => {
+    res.send("HAAHAHHAHAHA");
+})
+
+
+//GET user
+
+app.get("/user", (req, res) => {
+
+    res.send({
+        firstName:"Karthik",
+        lastName:"Kumar"
+    })
 });
+
+app.post("/user", (req, res) => {
+    
+    //save data to the database
+    res.send("Data is saved successfully to the database");
+
+})
+
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+})
+
+
+
  
 app.use("/test", (req, res) => {
     // this function is called as request handler
     res.send("Hello from the server!!!");
 });
 
-app.use("/hello", (req,res) => {
-    res.send("hello hello hello!!");
-});
+
+// app.use("/hello/1", (req,res) => {
+//     res.send("abraca dabra");
+// });
+
+
+// app.use("/hello", (req,res) => {
+//     res.send("hello hello hello!!");
+// });
+
+
+
+// app.use("/", (req, res) => {
+//     res.send("Namaste!");
+// });
 
 
 app.listen(7777, () => {
