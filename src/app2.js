b207e30797bd2a8f4ee09369df9298fbbcb38877
Sravani-Kumari-M <160271528+Sravani-Kumari-M@users.noreply.github.com>
2024-10-14 08:04:54 +0530

    delete, update & get user

--- src/app2.js
+++ src/app2.js
@@ -8,13 +8,114 @@ const User = require("./models/user");
 
 app.use(express.json())
 
+
+// find a user from the database
+
+app.get("/user", async (req, res) => {
+
+    const userEmail = req.body?.emailID;
+    console.log(userEmail);
+
+    try {
+        //const user = await User.find({emailID: userEmail});
+        // const user = await User.findOne({emailID: userEmail});
+        const user = await User.findOne();
+        // if(user.length === 0) {
+        //     res.status(404).send("User not found");
+        // } else {
+        //     res.send(user);
+        // }
+
+        if(!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+       
+    } catch(err) {
+        res.status(500).send("Something went wrong "+ err.message);
+    }
+})
+
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body?.userId;
+
+    try {
+        //const response = await User.findByIdAndDelete({_id: userId});
+        const response = await User.deleteMany({});
+        console.log("Response ", response);
+        if(!response) {
+            res.status(404).send("User not found");
+        } else {
+            res.send("user deleted successfully");
+        }
+        
+        
+    } catch(err) {
+        res.status(500).send("Error "+ err.message);
+    }
+})
+
+
+app.get("/feed", async (req, res) => {
+    console.log("calling feed api");
+
+    try {
+        const users = await User.find({});
+        console.log("users", users);
+        res.send(users);
+    } catch(err) {
+        res.status(500).send("Something went wrong "+ err.message);
+    }
+})
+
+//updating the data of the user
+app.patch("/user", async (req, res) => {
+
+    //const userId = req.body?.userId;
+    const emailID = req.body?.emailID;
+    
+    
+
+    try {
+
+        const user = await User.findOne({emailID: emailID});
+        let userId = user._id;
+        const response = await User.findByIdAndUpdate(userId, req.body, {
+            new:true,
+            lean:true,
+            select: {
+                _id:0,
+                firstName:1,
+                lastName:1,
+                emailID:1,
+                password:1
+            }
+
+        });
+        console.log("response after updating ", response);
+
+        if(!response) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(response);
+        }
+        
+        
+    } catch(err) {
+        res.status(500).send("Something went wrong " + err.message);
+    }
+})
+
+
 app.post("/signup", async (req, res) => {
 
-    console.log("request ==> ", req);
     const user = new User(req.body);
 
     try {
-        await user.save();
+        const response = await user.save();
+        console.log("response in creating => ", response);
         res.send("User created successfully");
     } catch(err) {
         res.status(400).send("Error in saving the user" + err.message);
