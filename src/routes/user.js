cac4ac82cec0ce80b41df36057f5329ea47d878d
Sravani-Kumari-M <160271528+Sravani-Kumari-M@users.noreply.github.com>
2024-10-20 20:37:59 +0530

    Implementing feed API with pagination & limit

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ const userRouter = express.Router();
 
 const {userAuth} = require("../middlewares/auth");
 const connectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 //find all the pending connection requests for the loggedin User
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -57,6 +58,55 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         res.status(400).send("Error " + err.message);
     }
    
+})
+
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+
+    try {
+        
+    const loggedinUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50: limit;
+
+    let skip = (page-1)*limit
+
+    //find all connection requests send r received by the user
+    const connectionRequests = await connectionRequest.find({
+        $or: [
+            {fromUserId: loggedinUser._id},
+            {toUserId: loggedinUser._id}
+        ]
+    })
+    .populate("fromUserId","firstName lastName")
+    .populate("toUserId", "firstName lastName")
+
+    const alreadyConnectedUsers = new Set();
+
+    connectionRequests.map((user) => {
+        alreadyConnectedUsers.add(user.fromUserId._id.toString());
+        alreadyConnectedUsers.add(user.toUserId._id.toString());
+    })
+
+    console.log("connected users ", alreadyConnectedUsers);
+
+    const usersToDisplay = await User.find({
+        $and: [
+            {_id: {$nin: Array.from(alreadyConnectedUsers)}},
+            {_id: {$ne: loggedinUser._id}}
+        ]
+    })
+    .skip(skip)
+    .limit(limit)
+    //console.log("connection request ", connectionRequests);
+
+    res.json(usersToDisplay);
+    } catch(err) {
+        res.status(400).json({message: "Eror "+ err.message})
+    }
+
+
 })
 
 module.exports = userRouter;
